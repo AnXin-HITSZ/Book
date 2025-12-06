8832cf3680b9b9a727a7798bfdd06ffb8231a8ad
AnXin-HITSZ <anxin.hitsz@outlook.com>
2025-12-06 11:34:06 +0800

    Notes - 20251206

---
+++ 深度学习入门-基于Python的理论与实现_斋藤康毅/src/chapter04/4.2_损失函数/4.2.1.均方误差.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# 实现均方误差
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+# 设 “2” 为正确解
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+# 例 1：“2” 的概率最高的情况（0.6）
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(mean_squared_error(np.array(y), np.array(t))) # 输出：0.09750000000000003
+
+# 例 2：“7” 的概率最高的情况（0.6）
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(mean_squared_error(np.array(y), np.array(t))) # 输出：0.5975
